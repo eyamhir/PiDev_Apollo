93f97d7a6a999b61ee59ffd4167001fd55afe411
radhwen grami <radhwen grami>
2024-02-24 23:04:54 +0100

    creation d'interface "AjouterPanier"

---
+++ src/main/java/tn/esprit/controllers/AjouterPanier.java
@@ -0,0 +1,96 @@
+package tn.esprit.controllers;
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+
+public class AjouterPanier {
+
+    @FXML
+    private ImageView dec_deuxieme_ouevre;
+
+    @FXML
+    private Button dec_premier_ouevre;
+
+    @FXML
+    private Button dec_troisieme_ouevre;
+
+    @FXML
+    private Button inc_deuxieme_ouevre;
+
+    @FXML
+    private Button inc_premier_ouevre;
+
+    @FXML
+    private Button inc_troisieme_ouevre;
+
+    @FXML
+    private Button payer;
+
+    @FXML
+    private ImageView remove_deuxieme_ouevre;
+
+    @FXML
+    private ImageView remove_premier_ouevre;
+
+    @FXML
+    private Button remove_troisieme_ouevre;
+
+
+    @FXML
+    void decrumenter_deuxieme_ouevre(MouseEvent event) {
+
+    }
+
+    @FXML
+    void decrumenter_premier_ouevre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void decrumenter_troisieme_ouevre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void incrumenter_deuxieme_ouevre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void incrumenter_premier_nombre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void incrumenter_troisieme_ouevre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void remove_deuxieme_ouevre(MouseEvent event) {
+
+    }
+
+    @FXML
+    void remove_premier_ouevre(MouseEvent event) {
+
+
+    }
+
+    @FXML
+    void remove_troisieme_ouevre(ActionEvent event) {
+
+    }
+
+    @FXML
+    void valider_payer(ActionEvent event) {
+
+
+    }
+
+
+}
