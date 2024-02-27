99d10db626d198c9cbb8493e27f3e68909232190
radhwen grami <radhwen grami>
2024-02-27 14:13:11 +0100

    Modification sur des liste Payment et Panier et commande

--- src/main/java/tn/esprit/controllers/ListePayment.java
+++ src/main/java/tn/esprit/controllers/ListePayment.java
@@ -1,7 +1,10 @@
 package tn.esprit.controllers;
 
+import com.google.zxing.WriterException;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import tn.esprit.models.Payment;
 import tn.esprit.sevices.PaymentService;
@@ -25,6 +28,33 @@ public class ListePayment {
     void backPay(ActionEvent event) throws IOException {
         MainFX main = new MainFX();
         main.changeStage("/tn.esprit/AjouterPayment.fxml");
+    }
+
+    @FXML
+    void generQRCode(ActionEvent event) {
+        ObservableList<Payment> payments = ListePayment.getItems();
+        for (Payment payment : payments) {
+            // Générer le contenu du code QR pour chaque paiement
+            String qrContent = "Payment ID: " + payment.getId_Payment() + ", Amount: " + payment.getMontant() + ", Type: " + payment.getType_Payment();
+            try {
+                // Générer le code QR pour ce paiement
+                QRCodeGenerator.generateQRCode(qrContent, "C:/Users/LENOVO/OneDrive/Bureau/Esprit S2/QRCodePayment/QRPayment.png" + payment.getId_Payment() + ".png");
+                System.out.println("Code QR généré pour le paiement avec l'ID : " + payment.getId_Payment());
+            } catch (WriterException | IOException e) {
+                System.err.println("Erreur lors de la génération du code QR pour le paiement avec l'ID : " + payment.getId_Payment());
+                e.printStackTrace();
+            }
+        }
+        showAlert("Success", "Codes QR générés avec succès.");
+    }
+
+
+    private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
     }
 
 }
